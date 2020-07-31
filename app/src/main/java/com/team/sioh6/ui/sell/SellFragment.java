3ab13f3de38f668375a63db74d3e5a0ac405a8b3
surajkrmodi <modisuraj61@gmail.com>
2020-08-01 01:17:26 +0530

    ADDED MENU OPTION IN SELL

--- app/src/main/java/com/team/sioh6/ui/sell/SellFragment.java
+++ app/src/main/java/com/team/sioh6/ui/sell/SellFragment.java
@@ -2,9 +2,12 @@ package com.team.sioh6.ui.sell;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -19,8 +22,16 @@ public class SellFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_sell, container, false);
+        //Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar_sell);
+        setHasOptionsMenu(true);
         final TextView textView = root.findViewById(R.id.text_sell);
         textView.setText("This is Selling fragment");
         return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.sell_menu, menu);
     }
 }
